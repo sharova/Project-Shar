aa15a462c6e521148074c139704b9650d5f00289
sharova <sv_sharova@mail.ru>
2015-10-13 17:01:47 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-$(document).ready(function() {
- 
-  //Sort random function
-  function random(owlSelector){
-    owlSelector.children().sort(function(){
-        return Math.round(Math.random()) - 0.5;
-    }).each(function(){
-      $(this).appendTo(owlSelector);
-    });
-  }
- 
-  $("#owl-demo").owlCarousel({
-    items : 4,
-    navigation: true,
-    navigationText: [
-      "<i class='icon-chevron-left icon-white'></i>",
-      "<i class='icon-chevron-right icon-white'></i>"
-      ],
-    beforeInit : function(elem){
-      //Parameter elem pointing to $("#owl-demo")
-      random(elem);
-    }
- 
-  });
- 
-});
-
-  
+$(document).ready(function(){
+      $('.owl-carousel').owlCarousel({
+      items:4,
+      loop:true,
+      mouseDrag: true,
+      touchDrag: true,
+      nav: true,
+      margin: 20,
+      navText: [" " , " "],
+      });
+      });
